c125d2a90cd8177b3687038329715d0cdd2d0962
DerrickMac <macarand@oregonstate.edu>
2023-10-20 00:52:56 -0700

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require('./database/db');
+const User = require("./models/userModel");
+const app = express();
+
+require('dotenv').config()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+connectDB();
+
+// Homepage
+app.get("/", async function (req, res) {
+    res.send('Hello World!')
+});
+
+// routes for authentication?
+app.get("/login", async function (req, res) {});
+
+app.get("/register", async function (req, res) {});
+
+// Demo for pulling all user data
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Demo for pulling user by ID
+app.get("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Demo for registering new user
+app.post("/register", async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = await User.create(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Demo for editing user info
+app.put("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, req.body);
+
+    if (!user) {
+      return res.status(404).json({ message: `user ID ${id} does not exist` });
+    }
+    const updatedUser = await User.findById(id);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Demo for deleting user
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ message: `user ID ${id} does not exist` });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+  
+app.listen(process.env.PORT, () => {
+console.log(`Node.js app running on port ${process.env.PORT}`);
+});
---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email missing"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password missing"],
+  },
+  fName: {
+    type: String,
+  },
+  lName: {
+    type: String,
+  },
+  profilePicture: {
+    type: String,
+  },
+  settings: {
+    type: Object,
+  }
+});
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
