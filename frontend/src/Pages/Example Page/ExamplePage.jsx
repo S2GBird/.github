f5c824cbe6cfb41a7cc1ea51a560551f98e7e8a1
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-24 12:17:21 -0400

    Downloaded the dev dependencies for the testing environment on frontend, Created an example test

--- frontend/src/Pages/Example Page/ExamplePage.jsx
+++ frontend/src/Pages/Example Page/ExamplePage.jsx
@@ -3,7 +3,7 @@ import ExampleStyles from './Example.module.css';
 export default function ExamplePage() {
   return (
     <div className={ExampleStyles.Example}>
-      HELLO WORLD, THIS IS THE EXAMPLE PAGE
+      <h2 data-testid = 'example-heading'>HELLO WORLD, THIS IS THE EXAMPLE PAGE</h2>
     </div>
   );
 };
