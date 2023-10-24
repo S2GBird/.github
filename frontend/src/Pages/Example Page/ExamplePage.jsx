f1d2b648b858190e61b34f76b2b06247bfadb9ad
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-24 15:19:29 -0400

    Downloaded dependency and added a cors policy handler to the app.js file to prevent cors blocking network errors when making calls from frontend to backend, Added comments to the examplePage.jsx file to give better explanation to how to structure future code/components,

--- frontend/src/Pages/Example Page/ExamplePage.jsx
+++ frontend/src/Pages/Example Page/ExamplePage.jsx
@@ -1,25 +1,29 @@
 import ExampleStyles from './Example.module.css';
 import apiClient from '../../Services/apiClient';
-import { useState } from 'react';
+import { useState, memo } from 'react';
 
-export default function ExamplePage() {
+function ExamplePage() {
   const [message, setMessage] = useState('');
   
+  //FUNCTION : When the button is clicked, send a call to the backend to get a message from backend health check
   const fetchExampleMessage = async () => {
-    const { data, error} = await apiClient.healthCheck();
-    if(data){
-      console.log(data);
+    const { data, error} = await apiClient.healthCheck(); //Use api client to call the endpoint I want
+    if(data){ //If I get data back (successful request)
+      setMessage(data) //Place functionality here
     }
-    if(error){
-      console.log(error);
+    if(error){ //If I get an error instead from the backend (unsuccessful request)
+      console.log(error); //Print my error
     }
   };
 
   return (
-    <div className={ExampleStyles.Example}>
+    <div className={ExampleStyles.Example}> {/**Example on how to attach your css styling to these components */}
       <h2>HELLO WORLD, THIS IS THE EXAMPLE PAGE</h2>
       <button onClick={fetchExampleMessage}> Click this button to show message</button>
       {message && <p>This is my message: { message }</p>}
     </div>
   );
 };
+
+//Explanation : Only apply the memo command when exporting your component if you are using variables that will constantly refresh or change such as useStates etc.
+export default memo(ExamplePage);
