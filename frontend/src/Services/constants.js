af74804fabafbd8e37dcbb2590d54d8511ffbc16
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-24 15:46:59 -0400

    Cleaning up the code

--- frontend/src/Services/constants.js
+++ frontend/src/Services/constants.js
@@ -1,4 +1,4 @@
-const PRODUCTION_API_BASE_URL = ''; //Backend Host URL
+//const PRODUCTION_API_BASE_URL = ''; //Backend Host URL
 const DEVELOPMENT_API_BASE_URL = 'http://localhost:3001' //Backend Dev LocalHost
 
 const API_BASE_URL = DEVELOPMENT_API_BASE_URL;
