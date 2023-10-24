53107daf563be1f4c7489520c81b30faca583152
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-24 11:09:41 -0400

    Created a mini example.jsx file to show the style in which other future files should be created, Adjusted the api client to have a health check request route to use within the example.jsx file to show how future axios requests should be made , Adjusted css files to be module.css to prevent class name clashes

---
+++ frontend/src/Pages/Example Page/ExamplePage.jsx
@@ -0,0 +1,9 @@
+import ExampleStyles from './Example.module.css';
+
+export default function ExamplePage() {
+  return (
+    <div className={ExampleStyles.Example}>
+      HELLO WORLD, THIS IS THE EXAMPLE PAGE
+    </div>
+  );
+};
