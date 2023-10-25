0e0298d92b1bcea4f83c1456b7e5828a836cf3bb
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-24 22:23:16 -0400

    Ran standard --fix to resolve some of the more detailed linting issues

--- frontend/src/Pages/App/App.jsx
+++ frontend/src/Pages/App/App.jsx
@@ -1,16 +1,15 @@
-import AppStyles from './App.module.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ExamplePage from '../Example Page/ExamplePage';
+import AppStyles from './App.module.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ExamplePage from '../Example Page/ExamplePage'
 
-
-export default function App() {
+export default function App () {
   return (
     <div className={AppStyles['app-page']}>
       <BrowserRouter>
         <Routes>
-          <Route path='/example' element={<ExamplePage />}></Route> {/**This is an example route, and the style to which future routes should follow. */}
+          <Route path='/example' element={<ExamplePage />} /> {/** This is an example route, and the style to which future routes should follow. */}
         </Routes>
       </BrowserRouter>
     </div>
-  );
+  )
 };
--- frontend/src/Services/apiClient.js
+++ frontend/src/Services/apiClient.js
@@ -2,38 +2,38 @@ import axios from 'axios'
 import API_BASE_URL from './constants'
 
 class ApiClient {
-    constructor(remoteHostUrl){
-        this.remoteHostUrl = remoteHostUrl; 
+  constructor (remoteHostUrl) {
+    this.remoteHostUrl = remoteHostUrl
+  }
+
+  // ----------------------GENERAL REQUEST HANDLING -----------------------------
+  /* Purpose: To standardize the method at which axios requests are made from frontend to backend, and
+    centralize the error handling so it does not need to be handled within the individual components. */
+
+  async request ({ endpoint, method = 'GET', data = {} }) {
+    const url = `${this.remoteHostUrl}/${endpoint}`
+    const headers = {
+      'Content-Type': 'application/json'
+      // Future Authorization Headers will go here.
     }
 
-    //----------------------GENERAL REQUEST HANDLING -----------------------------
-    /*Purpose: To standardize the method at which axios requests are made from frontend to backend, and 
-    centralize the error handling so it does not need to be handled within the individual components.*/
-
-    async request({ endpoint, method = 'GET', data={} }){
-        const url = `${this.remoteHostUrl}/${endpoint}`;
-        const headers = {
-            "Content-Type": "application/json",
-            //Future Authorization Headers will go here.
-        }
-
-        try{
-            //If a request is sucessful, send back an object containing the response
-            const response = await axios({ url, method, data, headers })
-            return { data: response.data, error: null }
-        } catch (error){
-            //If a request is unsucessful, send back an object containing the error message
-            console.error({errorResponse: error})
-            const message = error?.response?.data?.error?.message;
-            return { data: null, error: message || String(error) }
-        }
+    try {
+      // If a request is sucessful, send back an object containing the response
+      const response = await axios({ url, method, data, headers })
+      return { data: response.data, error: null }
+    } catch (error) {
+      // If a request is unsucessful, send back an object containing the error message
+      console.error({ errorResponse: error })
+      const message = error?.response?.data?.error?.message
+      return { data: null, error: message || String(error) }
     }
+  }
 
-    //-------------------------BACKEND HEALTH CHECK REQUESTS------------------------------
-    async healthCheck() {
-        return await this.request({ endpoint: ``, method: `GET` })
-    }
-    //-------------------------AUTHENTICATION REQUESTS------------------------------
+  // -------------------------BACKEND HEALTH CHECK REQUESTS------------------------------
+  async healthCheck () {
+    return await this.request({ endpoint: '', method: 'GET' })
+  }
+  // -------------------------AUTHENTICATION REQUESTS------------------------------
 }
 
-export default new ApiClient(API_BASE_URL);
+export default new ApiClient(API_BASE_URL)
