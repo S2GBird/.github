c04fad26b7f30497a273bd8ce0c411f3c10566c5
Vanessa Tang <vanesst5@uci.edu>
2023-10-25 20:31:47 -0700

    Added function to handle form submit ------------------------ set different error messages for cases like missing email/password and incorrect email/password will be adjusted when other parts are complete

--- frontend/src/Pages/Login Page/LoginPage.jsx
+++ frontend/src/Pages/Login Page/LoginPage.jsx
@@ -1,11 +1,36 @@
 import LoginStyles from './Login.module.css'
 // import apiClient from '../../Services/apiClient'
-import { useState} from 'react'
+import { useState, memo } from 'react'
 
 function LoginPage () {
 //   const [message, setMessage] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [valid, setValid] = useState(0) 
+
+  function handleLogin(event) {
+    event.preventDefault()
+    setError('')
+    // check to see if both email and password were entered
+      // return error message if either were missing
+    if(email !== "" && password !== "") {
+      // check if email and password are valid w/ db
+      // if valid redirect to dashboard
+      // return error message if incorrect email or password
+      setValid(1) // temporary 
+      if(valid) { 
+        console.log(email, password)
+        console.log("redirect to dashboard")
+      }
+      else {
+        setError('incorrect email or password')
+      } 
+    }
+    else {
+      setError('missing email or password')
+    } 
+  }
 
 //   // FUNCTION : When the login button is clicked, send a call to the backend to verify the information is correct
   // const login = async () => {
@@ -26,7 +51,7 @@ function LoginPage () {
       <div className={LoginStyles['login-header']}>
         <div className={LoginStyles['login-header-name']}>
           <div>Logo</div>
-          <div>Name of App</div>
+          <div>Chirp</div>
         </div>
         <ul className={LoginStyles['login-header-info']}>
           <li><a href='/#'>About</a></li>
@@ -37,13 +62,16 @@ function LoginPage () {
       <div className={LoginStyles['login-body-container']}>
         <div className={LoginStyles['login-body']}>
           <div>Picture</div>
-          <form className={LoginStyles['login-form']}>
-            <input type='text' value={email} onChange={(event) => setEmail(event.target.value)}></input>
-            <label>Email</label>
-            <input type='password' value={password} onChange={(event) => setPassword(event.target.value)}></input>
-            <label>Password</label>
-            <button>Login</button>
-          </form>
+          <div>
+            <form className={LoginStyles['login-form']} onSubmit={handleLogin}>
+              <input type='text' value={email} onChange={(event) => setEmail(event.target.value)}></input>
+              <label>Email</label>
+              <input type='password' value={password} onChange={(event) => setPassword(event.target.value)}></input>
+              <label>Password</label>
+              <button>Login</button>
+            </form>
+            <p>{error}</p>
+          </div>
         </div>
       </div>
       {/* <button onClick={fetchExampleMessage}> Click this button to show message</button>
@@ -54,4 +82,4 @@ function LoginPage () {
 };
 
 // Explanation : Only apply the memo command when exporting your component if you are using variables that will constantly refresh or change such as useStates etc.
-export default LoginPage
+export default memo(LoginPage)
