369af12e7742c1d126c64b1baf58ef3c9af40cbb
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2023-10-30 12:41:26 -0400

    Created basic structure for the landing page

--- frontend/src/Pages/App/App.jsx
+++ frontend/src/Pages/App/App.jsx
@@ -1,12 +1,14 @@
 import AppStyles from './App.module.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ExamplePage from '../Example Page/ExamplePage'
+import LandingPage from '../Landing Page/LandingPage'
 
 export default function App () {
   return (
     <div className={AppStyles['app-page']}>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<LandingPage/>} />
           <Route path='/example' element={<ExamplePage />} /> {/** This is an example route, and the style to which future routes should follow. */}
         </Routes>
       </BrowserRouter>
---
+++ frontend/src/Pages/Landing Page/LandingPage.jsx
@@ -0,0 +1,11 @@
+import LandingStyle from './LandingPage.module.css';
+
+function LandingPage(){
+    return(
+        <div className={LandingStyle['landing-page']}>
+            whoop di scoop
+        </div>
+    )
+};
+
+export default LandingPage;
