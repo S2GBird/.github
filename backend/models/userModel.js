7f925e3239ac45279cea370566c2c253d82c5b50
Fledtrain <Joeyew2014@gmail.com>
2023-10-31 14:21:07 -0400

    Added basic email validation in userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Email missing'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Invalid email format'],
     unique: true
   },
   password: {
