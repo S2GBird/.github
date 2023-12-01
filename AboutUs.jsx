60d882f5dbb9c4bea67fd16c74ffd1aefdda6165
Slouis07 <142813774+Slouis07@users.noreply.github.com>
2023-11-30 22:38:59 -0500

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import AboutUsStyles from './AboutUs.module.css'; 
+
+const AboutUs = () => {
+  return (
+    <div className={AboutUsStyles['aboutUsContainer']}>
+      <div className={AboutUsStyles['aboutSection']}>
+        <h1>About Us</h1>
+        {<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <title> chirp website aboutus</title>
+</head>
+  
+
+<body>
+
+
+  <header>
+      <nav class="navbar">
+          <div class="logo">
+            <img src = "birdlogo.png" alt="bird logo">
+          </div>
+          <div class="menu">
+              <ul>
+                  <li><a href="#">AboutUs</a></li>
+                  (li>Terms & Conditions<li)
+              </ul>
+          </div>
+        <div class="search-container">
+            <input type="text" class="search-input" placeholder="Search...">
+            <button class="search-button">Search</button>
+        </div>
+      </nav>
+  </header>
+  <div class="Aboutus-move">
+  <div class="aboutsection">
+    <div class="abouttext">
+      
+
+
+
+
+
+      
+      <h1>About Us</h1>
+
+       <div class="paragraph">
+      <p>Have you ever been told to touch grass? Encouraged to go explore nature? Tired of staring at your computer all day? Looking for something fun, inexpensive, and can be done anywhere, anytime? Then, you can’t go wrong with some bird watching! The bird-watching community is huge and a lot of enthusiasts do it purely for fun by ”keeping a life list - a birding term for the running list of all the different birds they see.” However, keeping in touch with the bird-watching community and finding people who enjoy this hobby just as much as you do is incredibly difficult especially as a newcomer just wanting to learn more about bird-watching or birding veterans wanting to share all the awesome adventures they’ve been on. What if we brought the community together through a social media app made just for birdwatchers?
+</p>
+         <br>
+  
+         
+      <div class="two-column-container">
+      <div class="visionphoto">
+      <img src="birdvisionphoto.png" alt="bird vision photo">
+      </div>
+      <div class="visiontext">
+        <h2>Our Vision</h2>
+      <p>  Our app, Chirp, brings that community aspect by encouraging bird enthusiasts to share posts about their birds, learn new things, find the best places to bird watch, and participate in events made just for the community! Let’s make bird watching a hobby for everyone!</p>
+    
+  
+ 
+</html>}
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
+
+
+import React from 'react';
+import AboutUs from './AboutUs';  // Adjust the path based on your folder structure
+
+const App = () => {
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  );
+};
+
+export default App;
+
+
