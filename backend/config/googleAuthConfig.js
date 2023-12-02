68abd2856730011d065a3312b96f7f53b4343faa
donnylp <dondariolapointe8@gmail.com>
2023-12-02 15:27:38 -0500

    fix linter

--- backend/config/googleAuthConfig.js
+++ backend/config/googleAuthConfig.js
@@ -1,40 +1,39 @@
 const passport = require('passport')
 const User = require('../models/userModel')
-const GoogleOIDCStrategy = require('passport-google-oidc').Strategy;
+const GoogleOIDCStrategy = require('passport-google-oidc').Strategy
 
 passport.use(new GoogleOIDCStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_REDIRECT,
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_REDIRECT
 },
-async function(accessToken, profile, done){
-    try{
-    //check if user exists in database
-        const user = await User.findOne({email: profile.emails[0].value});
-       
-        if(user){
-            //log the user in
-            done(null,user);
-        }
-        else{
-            const newUser = new User({
-            username: profile.displayName,
-            email: profile.emails[0].value,
-            fName: profile.name.givenName,
-            lName: profile.name.familyName,
-            profilePicture: '',
-            settings: {
-                dark: false
-            }
-        });
+async function (accessToken, profile, done) {
+  try {
+    // check if user exists in database
+    const user = await User.findOne({ email: profile.emails[0].value })
 
-        //save user to the database
-        await newUser.save();
-
-        //log the new user in
-        done(null,newUser);
+    if (user) {
+      // log the user in
+      done(null, user)
+    } else {
+      const newUser = new User({
+        username: profile.displayName,
+        email: profile.emails[0].value,
+        fName: profile.name.givenName,
+        lName: profile.name.familyName,
+        profilePicture: '',
+        settings: {
+          dark: false
         }
-    }catch (error){
-    done(error);
-}
-}));
+      })
+
+      // save user to the database
+      await newUser.save()
+
+      // log the new user in
+      done(null, newUser)
+    }
+  } catch (error) {
+    done(error)
+  }
+}))
