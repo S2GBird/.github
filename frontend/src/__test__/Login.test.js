249df2b32e5a6263dd8e8ef41eae19d9c4a164c5
Jonas Ling <85148672+ItsJonasL@users.noreply.github.com>
2023-12-13 19:10:31 -0500

    Fixing linter

--- frontend/src/__test__/Login.test.js
+++ frontend/src/__test__/Login.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import LoginPage from 'frontend/src/Pages/Login Page/LoginPage.jsx';
-import '@testing-library/jest-dom';
-
+import React from 'react'
+import { render } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import LoginPage from 'frontend/src/Pages/Login Page/LoginPage.jsx'
+import '@testing-library/jest-dom'
 
 describe('LoginPage Component', () => {
   test('renders login form with necessary fields', () => {
@@ -11,16 +10,16 @@ describe('LoginPage Component', () => {
       <Router>
         <LoginPage />
       </Router>
-    );
+    )
 
     // Check for username input field
-    expect(getByLabelText(/Username/i)).toBeInTheDocument();
+    expect(getByLabelText(/Username/i)).toBeInTheDocument()
 
     // Check for password input field
-    expect(getByLabelText(/Password/i)).toBeInTheDocument();
+    expect(getByLabelText(/Password/i)).toBeInTheDocument()
 
     // Check for login button
-    expect(getByRole('button', { name: /Login/i })).toBeInTheDocument();
-    //more test will be added on. this at the moment is just for rendering login page and check of fields 
-  });
-});
+    expect(getByRole('button', { name: /Login/i })).toBeInTheDocument()
+    // more test will be added on. this at the moment is just for rendering login page and check of fields
+  })
+})
