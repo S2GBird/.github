3d2b85aceee8b07bfc994ec05998e37231fde2b8
Vanessa Tang <vanesst5@uci.edu>
2023-12-28 22:57:18 -0800

    Added function to retrieve user info

--- frontend/src/Services/apiClient.js
+++ frontend/src/Services/apiClient.js
@@ -40,6 +40,10 @@ class ApiClient {
     // username and password is sent to backend to check if user exists/entered the correct information
     return await this.request({ endpoint: 'login', method: 'POST', data: { username: user.username, password: user.password } })
   }
+
+  async getUserInfo (userId) {
+    return await this.request({ endpoint:`users/${userId}`, method: 'GET' })
+  }
 }
 
 export default new ApiClient(API_BASE_URL)
