b8ebaa4bbce9f4ba5f42720e0f30570e7b5284b3
Vanessa Tang <vanesst5@uci.edu>
2024-01-05 12:23:25 -0800

    Retrieved user info based on user id

--- frontend/src/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import apiClient from '../../Services/apiClient'
+import { useAuthContext } from '../../Services/authProvider'
+import { useState, memo } from 'react'
+import DashboardStyles from './Dashboard.module.css'
 
 function Dashboard () {
+    const { user } = useAuthContext()
+    const [username, setUsername] = useState(user.username)
+    const [userId, setUserId] = useState(user.userId)
+    const [email, setEmail] = useState('')
+    const [fName, setFName] = useState('')
+    const [lName, setLName] = useState('')
+
+    function getUserInfo(userId) {
+        apiClient.getUserInfo(userId).then(res => {
+            setEmail(res.data.email)
+            setFName(res.data.fName)
+            setLName(res.data.lName)
+        });
+    }
+
+    getUserInfo(user.userId)
+
     return (
-        <div>Dashboard</div>
+        <div className={DashboardStyles['text-center']}>
+            <div>Dashboard</div>
+            <div>User ID: { userId }</div>
+            <div>Email: { email }</div>
+            <div>Username: { username }</div>
+            <div>First Name: { fName }</div>
+            <div>Last Name: { lName }</div>
+        </div>
     )
 }
 
-export default Dashboard
+export default memo(Dashboard)
