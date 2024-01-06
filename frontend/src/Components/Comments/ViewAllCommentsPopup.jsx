472be1db6164dfd25e7764d383b4ead8bec6dae8
Vanessa Tang <vanesst5@uci.edu>
2024-01-05 16:05:55 -0800

    Restructured popup/modal

--- frontend/src/Components/Comments/ViewAllCommentsPopup.jsx
+++ frontend/src/Components/Comments/ViewAllCommentsPopup.jsx
@@ -1,7 +1,9 @@
 import Styles from './ViewAllCommentsPopup.module.css'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import Comment from './Comment.jsx'
+import { useState } from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 
 const comments = [
     {
@@ -67,20 +69,55 @@ const comments = [
 ]
 
 function ViewAllCommentsPopup ( { PostId } ) {
+    const [open, setOpen] = useState(false)
+
+    const popupContainer = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '35rem',
+        bgcolor: 'background.paper',
+        boxShadow: 24,
+        borderRadius: '0.5rem',
+        padding: '2rem'
+    }
+
+    function handleToggle() {
+        setOpen(!open)
+    }
 
     return (
         <div>
-            <div className={Styles['text-center']}>123 comments</div>
-            {
-                comments.map((comment) =>
-                    // <div className={Styles['comments-container']} >
-                    //     <div>{comment.username}</div>
-                    //     <div>{comment.comment}</div>
-                    // </div>
-                    <Comment username={comment.username }comment={comment.comment} />
-                )
-            }
-            
+            <button className={Styles['view-btn']} onClick={handleToggle}>View all comments</button>
+            <Modal
+                open={open}
+                onClose={handleToggle}
+            >
+                <Box sx={popupContainer}>
+                        <div className={Styles['text-center']}>123 comments</div>
+                        {
+                            comments.map((comment, i) =>
+                                // key is temporary will replace with _id later
+                                <Comment key={i} username={ comment.username } comment={ comment.comment } />
+                            )
+                        }
+                        <Button onClick={handleToggle}>Close</Button>                 
+                </Box>
+            </Modal>
+            {/* {
+                open ? (
+                    <div className={Styles['popup-container']}>
+                        <div className={Styles['text-center']}>123 comments</div>
+                        {
+                            comments.map((comment, i) =>
+                                // key is temporary will replace with _id later
+                                <Comment key={i} username={ comment.username } comment={ comment.comment } />
+                            )
+                        }
+                    </div>
+                ) : ""
+            } */}
         </div>
     )
 };
