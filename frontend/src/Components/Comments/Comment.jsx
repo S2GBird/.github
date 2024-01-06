541b84d243b144b795b3d8c3b1750ea529615831
Vanessa Tang <vanesst5@uci.edu>
2024-01-05 16:50:46 -0800

    Fixed spacing

--- frontend/src/Components/Comments/Comment.jsx
+++ frontend/src/Components/Comments/Comment.jsx
@@ -4,27 +4,27 @@ import FavoriteIcon from '@mui/icons-material/Favorite'
 import { useState } from 'react'
 
 function Comment ( { username, comment } ) {
-    const [like, setLike] = useState(false)
+  const [like, setLike] = useState(false)
 
-    function handleLike() {
-        setLike(!like)
-        // make call to backend to add or remove like from comment
-        // likes dont persist currently so that will have to be fixed later on
-        // need something to check if the comment is liked by the current user already
-    }
+  function handleLike() {
+    setLike(!like)
+    // make call to backend to add or remove like from comment
+    // likes dont persist currently so that will have to be fixed later on
+    // need something to check if the comment is liked by the current user already
+  }
 
-    return (
+  return (
+    <div>
+      <div className={Styles['comments-container']}>
         <div>
-            <div className={Styles['comments-container']}>
-                <div>
-                    <span><b>{username}</b> {comment}</span>
-                </div>
-                <div onClick={handleLike}>
-                    {like ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-                </div>
-            </div>
+          <span><b>{username}</b> {comment}</span>
         </div>
-    )
+        <div onClick={handleLike}>
+          {like ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Comment
--- frontend/src/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -5,33 +5,33 @@ import { useState, memo } from 'react'
 import DashboardStyles from './Dashboard.module.css'
 
 function Dashboard () {
-    const { user } = useAuthContext()
-    const [username, setUsername] = useState(user.username)
-    const [userId, setUserId] = useState(user.userId)
-    const [email, setEmail] = useState('')
-    const [fName, setFName] = useState('')
-    const [lName, setLName] = useState('')
+  const { user } = useAuthContext()
+  const [username, setUsername] = useState(user.username)
+  const [userId, setUserId] = useState(user.userId)
+  const [email, setEmail] = useState('')
+  const [fName, setFName] = useState('')
+  const [lName, setLName] = useState('')
 
-    function getUserInfo(userId) {
-        apiClient.getUserInfo(userId).then(res => {
-            setEmail(res.data.email)
-            setFName(res.data.fName)
-            setLName(res.data.lName)
-        });
-    }
+  function getUserInfo(userId) {
+    apiClient.getUserInfo(userId).then(res => {
+      setEmail(res.data.email)
+      setFName(res.data.fName)
+      setLName(res.data.lName)
+    });
+  }
 
-    getUserInfo(user.userId)
+  getUserInfo(user.userId)
 
-    return (
-        <div className={DashboardStyles['text-center']}>
-            <div>Dashboard</div>
-            <div>User ID: { userId }</div>
-            <div>Email: { email }</div>
-            <div>Username: { username }</div>
-            <div>First Name: { fName }</div>
-            <div>Last Name: { lName }</div>
-        </div>
-    )
+  return (
+    <div className={DashboardStyles['text-center']}>
+      <div>Dashboard</div>
+      <div>User ID: { userId }</div>
+      <div>Email: { email }</div>
+      <div>Username: { username }</div>
+      <div>First Name: { fName }</div>
+      <div>Last Name: { lName }</div>
+    </div>
+  )
 }
 
 export default memo(Dashboard)
