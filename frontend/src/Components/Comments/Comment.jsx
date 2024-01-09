f9abbdbe5d61c15f082de581142344220ad279bc
Vanessa Tang <vanesst5@uci.edu>
2024-01-09 00:14:42 -0800

    Added like count to comments

--- frontend/src/Components/Comments/Comment.jsx
+++ frontend/src/Components/Comments/Comment.jsx
@@ -1,16 +1,28 @@
 import Styles from './Comment.module.css'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import FavoriteIcon from '@mui/icons-material/Favorite'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-function Comment ( { username, comment } ) {
+function Comment ( { username, comment, likes } ) {
   const [like, setLike] = useState(false)
+  const [commentLikes, setCommentLikes] = useState(likes)
 
   function handleLike() {
+    updateLikes(!like)
     setLike(!like)
+  }
+
+  function updateLikes (like) {
     // make call to backend to add or remove like from comment
     // likes dont persist currently so that will have to be fixed later on
     // need something to check if the comment is liked by the current user already
+    if (like) {
+      setCommentLikes(commentLikes + 1)
+    }
+    else {
+      setCommentLikes(commentLikes - 1)
+    }
+    console.log(commentLikes)
   }
 
   return (
@@ -19,8 +31,11 @@ function Comment ( { username, comment } ) {
         <div>
           <span><b>{username}</b> {comment}</span>
         </div>
-        <div onClick={handleLike}>
-          {like ? <FavoriteIcon style={{color: 'red'}} /> : <FavoriteBorderIcon />}
+        <div className={Styles['likes-container']}>
+          <div className={Styles['likes']}>{commentLikes === 0 ? '' : commentLikes}</div>
+          <div onClick={handleLike}>
+            {like === true ? <FavoriteIcon style={{color: 'red'}} /> : <FavoriteBorderIcon />}
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/Components/Comments/ViewAllCommentsPopup.jsx
+++ frontend/src/Components/Comments/ViewAllCommentsPopup.jsx
@@ -13,63 +13,78 @@ import { useAuthContext } from '../../Services/authProvider'
 const test = [
   {
     username: 'user1',
-    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    likes: 4
   },
   {
     username: 'user2',
-    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    likes: 6
   },
   {
     username: 'user3',
-    comment: 'comment3'
+    comment: 'comment3',
+    likes: 5
   },
   {
     username: 'user4',
-    comment: 'comment4'
+    comment: 'comment4',
+    likes: 9
   },
   {
     username: 'user5',
-    comment: 'comment5'
+    comment: 'comment5',
+    likes: 0
   },
   {
     username: 'user1',
-    comment: 'comment1'
+    comment: 'comment1',
+    likes: 2
   },
   {
     username: 'user2',
-    comment: 'comment2'
+    comment: 'comment2',
+    likes: 8
   },
   {
     username: 'user3',
-    comment: 'comment3'
+    comment: 'comment3',
+    likes: 0
   },
   {
     username: 'user4',
-    comment: 'comment4'
+    comment: 'comment4',
+    likes: 0
   },
   {
     username: 'user5',
-    comment: 'comment5'
+    comment: 'comment5',
+    likes: 0
   },
   {
     username: 'user1',
-    comment: 'comment1'
+    comment: 'comment1',
+    likes: 0
   },
   {
     username: 'user2',
-    comment: 'comment2'
+    comment: 'comment2',
+    likes: 0
   },
   {
     username: 'user3',
-    comment: 'comment3'
+    comment: 'comment3',
+    likes: 0
   },
   {
     username: 'user4',
-    comment: 'comment4'
+    comment: 'comment4',
+    likes: 57
   },
   {
     username: 'user5',
-    comment: 'comment5'
+    comment: 'comment5',
+    likes: 467
   }
 ]
 
@@ -127,7 +142,7 @@ function ViewAllCommentsPopup ( { postId } ) {
           {
             comments.map((comment, i) =>
               // key is temporary will replace with _id later
-              <Comment key={i} username={ comment.username } comment={ comment.comment } />
+              <Comment key={i} username={ comment.username } comment={ comment.comment } likes={comment.likes} />
             )
           }
         </DialogContent>
