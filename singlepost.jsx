79a952c0d3ae994de9dc0a94b06889cb001c53cf
Slouis07 <142813774+Slouis07@users.noreply.github.com>
2024-01-15 16:23:25 -0500

    singlepost.jsx

--- singlepost.jsx
+++ singlepost.jsx
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>replit</title>
+  <title>SinglePost</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
 </head>
